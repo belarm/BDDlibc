e6419ddb0d0ca31fd43a0e9053dee7b038c9a943
Steven M. Corbin II <belarm@users.noreply.github.com>
2016-08-26 01:11:47 -0400

    script to run test and generate graphs if graphviz is installed

---
+++ tests/make_graphs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir Debug/images
+cd Debug/images
+../tests
+for i in $(ls |grep dot$|xargs -n 1 basename -s .dot)
+do 
+	dot ${i}.dot -Tpng -o ${i}.png
+done
+cd -
+echo "images created in Debug/images/"
